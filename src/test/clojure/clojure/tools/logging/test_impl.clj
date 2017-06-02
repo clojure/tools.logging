3977d21cf67ccf012e880bda4d64b94e9116305b
Alexander Taggart <mail@ataggart.ca>
2017-06-01 20:09:30 -0400

    Allow non-standard levels for applicable impls.
    Fixes TLOG-18.

--- src/test/clojure/clojure/tools/logging/test_impl.clj
+++ src/test/clojure/clojure/tools/logging/test_impl.clj
@@ -14,3 +14,17 @@
 (deftest test-logger-impls-satisfy
   (doseq [lf logger-factories]
     (is (satisfies? impl/Logger (impl/get-logger lf "logger-ns")))))
+
+(deftest test-enabled-non-kw-levels
+  ; Just checking that the call doesn't blow up.
+  (are [lf level] (let [logger (impl/get-logger lf "logger-ns")]
+                    (impl/enabled? logger level))
+       (impl/log4j-factory) org.apache.log4j.Level/FATAL
+       (impl/jul-factory)   java.util.logging.Level/SEVERE))
+
+(deftest test-write-non-kw-levels
+  ; Just checking that the call doesn't blow up.
+  (are [lf level] (let [logger (impl/get-logger lf "logger-ns")]
+                    (nil? (impl/write! logger level nil "message")))
+       (impl/log4j-factory) org.apache.log4j.Level/FATAL
+       (impl/jul-factory)   java.util.logging.Level/SEVERE))
