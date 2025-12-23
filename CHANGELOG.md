1a02b1505c32bbf980ca3dff082f463519f1813e
Alex Miller <alex.miller@cognitect.com>
2025-12-23 12:34:15 -0600

    update deps to latest

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html)
 ([despite its flaws](https://www.youtube.com/watch?v=oyLBGkS5ICk)).
 
+## next
+
+### Changed
+
+- Bumped deps to Clojure 1.11.4
+- Bumped test deps to latest
+
 ## [1.3.0] - 2024-02-06
 
 ### Changed
--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.11.4"]]
   :aliases {"cloverage" ["with-profile" "dev,cloverage" "cloverage" "--fail-threshold" "100"]}
-  :profiles {:cloverage {:plugins [[lein-cloverage "1.0.9"]]}
-             :dev       {:dependencies [[org.clojure/clojure "1.10.3"]
-                                        [org.clojure/test.check "1.1.1"]
-                                        [org.slf4j/slf4j-api "1.7.32"]
-                                        [org.slf4j/slf4j-log4j12 "1.7.32"]
-                                        [org.apache.logging.log4j/log4j-api "2.17.1"]
-                                        [org.apache.logging.log4j/log4j-core "2.17.1"]
-                                        [commons-logging "1.2"]
+  :profiles {:cloverage {:plugins [[lein-cloverage "1.2.4"]]}
+             :dev       {:dependencies [[org.clojure/clojure "1.11.4"]
+                                        [org.clojure/test.check "1.1.2"]
+                                        [org.slf4j/slf4j-api "2.0.17"]
+                                        [org.slf4j/slf4j-log4j12 "2.0.17"]
+                                        [org.apache.logging.log4j/log4j-api "2.25.3"]
+                                        [org.apache.logging.log4j/log4j-core "2.25.3"]
+                                        [commons-logging "1.3.5"]
                                         [criterium "0.4.6"]]}})
